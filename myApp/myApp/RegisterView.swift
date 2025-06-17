451671224bf79d2dd4ca3840b9691d5727eca20f
GigaOwO <takumagiga1118@gmail.com>
2025-06-17 13:46:30 +0900

    フォームを追加

--- myApp/myApp/RegisterView.swift
+++ myApp/myApp/RegisterView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct RegisterView: View {
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var passwordConfirm: String = ""
     var body: some View {
         VStack {
             TextField(
@@ -27,6 +28,16 @@ struct RegisterView: View {
             .textFieldStyle(.plain)
             .padding(6)
             .background(Color(UIColor.systemFill), in: .rect(cornerRadius: 6))
+            TextField(
+                "パスワード確認",
+                text: $passwordConfirm
+            )
+            .textFieldStyle(.plain)
+            .padding(6)
+            .background(Color(UIColor.systemFill), in: .rect(cornerRadius: 6))
+            Button("新規登録") {
+                
+            }.disabled(email.isEmpty || password.isEmpty)
         }
         .textFieldStyle(.roundedBorder)
     }
