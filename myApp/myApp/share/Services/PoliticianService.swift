1f2a317e8200c3ae51fff8033c54b45c4d3c0229
kmjak <tymkoron1221@icloud.com>
2025-06-20 15:45:01 +0900

    refactor: jsonデータを返すようにした

--- myApp/myApp/share/Services/PoliticianService.swift
+++ myApp/myApp/share/Services/PoliticianService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct PoliticianService {
-    static func generatePolitician( completion: @escaping (Result<Void, Error>) -> Void) {
+    static func generatePolitician(completion: @escaping (Result<String, Error>) -> Void) {
         guard let url = URL(string: "http://localhost:8080/api/private/politicians/generate") else {
             completion(.failure(PoliticianServiceError.invalidURL))
             return
@@ -17,7 +17,8 @@ struct PoliticianService {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        var accessToken: String = KeychainHelper.load() ?? ""
+        
+        let accessToken: String = KeychainHelper.load() ?? ""
         request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
         
         URLSession.shared.dataTask(with: request) { data, response, error in
@@ -32,17 +33,16 @@ struct PoliticianService {
             }
             
             if httpResponse.statusCode == 200 {
-                if let data = data {
-                    // JSONとしてデコードせずに文字列として表示
-                    if let jsonString = String(data: data, encoding: .utf8) {
-                        print("受け取ったデータ: \(jsonString)")
-                    }
+                if let data = data, let jsonString = String(data: data, encoding: .utf8) {
+                    completion(.success(jsonString))
+                } else {
+                    completion(.success("成功したがデータなし"))
                 }
-                completion(.success(()))
             } else {
                 completion(.failure(PoliticianServiceError.serverError(statusCode: httpResponse.statusCode)))
             }
-        }.resume()    }
+        }.resume()
+    }
 }
 
 enum PoliticianServiceError: Error {
